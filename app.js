a8dd2b76820ef9dfb9f6a9ee3cd57a6ae902646b
Dlorodev <dlorodev@gmail.com>
2024-03-18 21:24:21 -0500

    feat: encrypt the entered text and display it in the results section

--- app.js
+++ app.js
@@ -2,3 +2,33 @@ function hideResultBefore() {
     document.getElementById('result-before').style.display = "none";
     document.getElementById('result-after').style.display = "flex";
 }
+
+
+//encrypt function
+function encrypt() {
+    let textarea1 = document.getElementById('textarea1');
+    let textToEncrypt = textarea1.value;
+    let textArray = [];
+    let textEncrypted = "";
+
+    for (let i = 0; i < textToEncrypt.length; i++) {
+        textArray.push(textToEncrypt[i]);
+    }
+
+    for (let j = 0; j < textArray.length; j++) {
+        if (textArray[j] == "e") {
+            textArray[j] = "enter";
+        } else if (textArray[j] == "i") {
+            textArray[j] = "imes";
+        } else if (textArray[j] == "a") {
+            textArray[j] = "ai";
+        } else if (textArray[j] == "o") {
+            textArray[j] = "ober";
+        } else if (textArray[j] == "u") {
+            textArray[j] = "ufat";
+        }
+    }
+
+    textEncrypted = textArray.join("");
+    document.getElementById('textarea2').value = textEncrypted;
+}
